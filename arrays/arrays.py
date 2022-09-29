b19875fe912a7762b15a6c16c9d2feabfa84860e
Lemmynjash <lemmynjariakiarie@gmail.com>
2022-09-29 09:44:21 +0300

    did a refresher on 2d arrays

--- arrays/arrays.py
+++ arrays/arrays.py
@@ -36,9 +36,35 @@ def twod_arrays():
             print(j)
 
 
+def exercise_one():
+    # initialize the 2d array
+    a_2d = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    # replace no 6 with 99
+    a_2d[1][1] = 99
+    print(a_2d[1][1])
+
+    # print
+    for i in a_2d:
+        for j in i:
+            print(j)
+
+    # diagonal elements
+    # which are 1,6,10
+    total = 0
+    for i in range(len(a_2d)):
+        total += a_2d[i][i]
+    print(total)
+
+
 if __name__ == "__main__":
-    print("print_position---> "+str(print_position()))
-    print("print_all---> "+str(print_all()))
-    instert_new()
-    remove_item()
-    twod_arrays()
+    #print("print_position---> "+str(print_position()))
+    #print("print_all---> "+str(print_all()))
+    # instert_new()
+    # remove_item()
+    # twod_arrays()
+    exercise_one()
